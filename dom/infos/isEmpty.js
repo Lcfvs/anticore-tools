0e52c1cf2b35f987d73354f78fe7299f3c2b8359
Lcfvs <michael.rouges@gmail.com>
2018-01-30 11:15:20 +0100

    Improving dom::infos::isEmpty

--- dom/infos/isEmpty.js
+++ dom/infos/isEmpty.js
@@ -1,5 +1,8 @@
 import {text} from './text';
+import {isElement} from './isElement';
+import {one} from '../queries/one';
 
 export function isEmpty(node) {
-  return !(text(node) || '').trim().length;
+  return (!isElement(node) || !one('*', node))
+  && !(text(node) || '').trim().length;
 }
