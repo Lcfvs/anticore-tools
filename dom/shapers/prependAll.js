868845a874ab32ff9d1a6e7e91dde2f83aa8e90f
Lcfvs <michael.rouges@gmail.com>
2018-02-02 23:11:11 +0100

    Improving prependAll

--- dom/shapers/prependAll.js
+++ dom/shapers/prependAll.js
@@ -1,9 +1,20 @@
 import {prepend} from './prepend';
 import {forEach} from '../../utils/array/forEach';
 import {slice} from '../../utils/array/slice';
+import {insert} from './insert';
 
 export function prependAll(nodes, parent) {
-  forEach(slice(nodes, 0), function(node) {
-    prepend(node, parent);
+  let
+  items = slice(nodes, 0),
+  first;
+
+  if (items.length) {
+    return;
+  }
+
+  first = prepend(items.shift(), parent);
+
+  forEach(items, function(item) {
+    insert(item, first.nextSibling, parent);
   });
 }
